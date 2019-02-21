aeba64f8eca9f337585d2c39d6f929246020429a
Seenivasan Sankaran <seenivasan.s@heymath.com>
2019-02-21 15:35:12 +0530

    Initial Commit

--- app/src/main/java/heymath/com/materialdesignlibrary/MainActivity.java
+++ app/src/main/java/heymath/com/materialdesignlibrary/MainActivity.java
@@ -1,7 +1,11 @@
 package heymath.com.materialdesignlibrary;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
--- heymathlibrary/src/main/java/heymath/com/heymathlibrary/ToasterMessage.java
+++ heymathlibrary/src/main/java/heymath/com/heymathlibrary/ToasterMessage.java
@@ -1,12 +1,36 @@
 package heymath.com.heymathlibrary;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class ToasterMessage {
+public class ToasterMessage extends AppCompatActivity {
     public static void s(Context c, String message) {
-
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
-
     }
+
+    public static void customSnakbar(final Context c, View v, String msg, String action) {
+        final Snackbar snackbar = Snackbar
+                .make(v, msg, Snackbar.LENGTH_LONG)
+                .setAction(action, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        s(c,"Ok");
+                    }
+                });
+
+// Changing message text color
+        snackbar.setActionTextColor(Color.RED);
+
+// Changing action button text color
+        View sbView = snackbar.getView();
+        TextView textView = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
+        textView.setTextColor(Color.YELLOW);
+        snackbar.show();
+    }
+
 }
