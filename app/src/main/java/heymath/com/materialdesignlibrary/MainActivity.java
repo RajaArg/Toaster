93c731864c1b66468efe9154e181abb49808173c
Seenivasan Sankaran <seenivasan.s@heymath.com>
2019-02-21 11:53:36 +0530

    Initial Commit

---
+++ app/src/main/java/heymath/com/materialdesignlibrary/MainActivity.java
@@ -0,0 +1,13 @@
+package heymath.com.materialdesignlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
